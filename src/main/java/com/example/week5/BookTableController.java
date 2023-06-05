e7ac3099e55885d6583ccd3dc748d6d297734ab8
Priyansh Thakar <priyanshthakar@gmail.com>
2023-06-05 11:35:13 -0400

    Added scene changer utility

--- src/main/java/com/example/week5/BookTableController.java
+++ src/main/java/com/example/week5/BookTableController.java
@@ -1,4 +1,44 @@
 package com.example.week5;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class BookTableController {
+    @FXML
+    private TableView<Book> tableView;
+
+    @FXML
+    private TableColumn<Book, String> authorColumn;
+
+    @FXML
+    private TableColumn<Book, Boolean> availableColumn;
+
+    @FXML
+    private TableColumn<Book, Integer> booksSoldColumn;
+
+    @FXML
+    private TableColumn<Book, String> genreColumn;
+
+    @FXML
+    private TableColumn<Book, Integer> idColumn;
+
+    @FXML
+    private TableColumn<Book, String> nameColumn;
+
+    @FXML
+    private TableColumn<Book, Double> priceColumn;
+
+
+    @FXML
+    void addNewBook_onClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void viewChart_onClick(ActionEvent event) {
+
+    }
+
 }
---
+++ src/main/java/com/example/week5/SceneChanger.java
@@ -0,0 +1,23 @@
+package com.example.week5;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SceneChanger {
+    /*
+    This will change the scene when called
+     */
+    public static void changeScenes(ActionEvent actionEvent, String fxmlFileName, String viewTitle) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(fxmlFileName));
+        Scene scene = new Scene(fxmlLoader.load());
+        Stage stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        stage.setTitle(viewTitle);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
