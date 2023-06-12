d8c05049514e799b7608abfd6ef0b4481c7d86b6
Priyansh Thakar <priyanshthakar@gmail.com>
2023-06-12 11:40:11 -0400

    Added styles and icon

--- src/main/java/com/example/week6/Main.java
+++ src/main/java/com/example/week6/Main.java
@@ -3,6 +3,7 @@ package com.example.week6;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,6 +14,7 @@ public class Main extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("create-book-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Save Book!");
+        stage.getIcons().add(new Image("file:src/main/resources/com/example/week6/favicon.png"));
         stage.setScene(scene);
         stage.show();
     }
